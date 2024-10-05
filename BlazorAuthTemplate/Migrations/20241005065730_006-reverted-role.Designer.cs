73e4730bc91fb2ea888d2d1b26c94b0eef744ac1
Todd <toddm2770@gmail.com>
2024-10-05 02:58:04 -0400

    reverted AppUser changes

--- BlazorAuthTemplate/Migrations/20241005065730_006-reverted-role.Designer.cs
+++ BlazorAuthTemplate/Migrations/20241005065730_006-reverted-role.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlazorAuthTemplate.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlazorAuthTemplate.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241005065730_006-reverted-role")]
+    partial class _006revertedrole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
