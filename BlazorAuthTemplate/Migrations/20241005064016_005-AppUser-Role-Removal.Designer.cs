2c0ca7fb39410f7723e07880c30b5f4d5cd2b40f
Todd <toddm2770@gmail.com>
2024-10-05 02:49:47 -0400

    fixed vendors files that weren't loading properly

--- BlazorAuthTemplate/Migrations/20241005064016_005-AppUser-Role-Removal.Designer.cs
+++ BlazorAuthTemplate/Migrations/20241005064016_005-AppUser-Role-Removal.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlazorAuthTemplate.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlazorAuthTemplate.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241005064016_005-AppUser-Role-Removal")]
+    partial class _005AppUserRoleRemoval
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
