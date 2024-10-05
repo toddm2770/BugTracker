ecb447113370c52566ec3dd10eb409cd05a9fcb6
Todd <toddm2770@gmail.com>
2024-10-05 16:42:57 -0400

    added migrations back

---
+++ BlazorAuthTemplate/Migrations/20241005065730_006-reverted-role.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAuthTemplate.Migrations
+{
+    /// <inheritdoc />
+    public partial class _006revertedrole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "AspNetUsers");
+        }
+    }
+}
